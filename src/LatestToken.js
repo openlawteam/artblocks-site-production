bde758c1aa77bbe43234c6422a1534325fcc00e2
sophiacodes <sophiacodes@gmail.com>
2021-06-02 21:17:54 +0100

    updated baseurl

--- src/LatestToken.js
+++ src/LatestToken.js
@@ -12,7 +12,6 @@ const LatestToken = ({project, complete, random, latest}) => {
   const tokenId = (complete ? random : latest) + project * 1000000;
   const tokenURL = tokenGenerator(tokenId);
 
-  console.log('>>> tokenURL', tokenURL);
   return (
     <div className="text-center">
       <a href={`/token/${tokenId}`}>
